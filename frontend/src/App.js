8535aa62b3cce47fb400fd520aa3f6b3e76694ee
Mana <vuvictor2003@gmail.com>
2024-05-10 16:33:45 -0700

    Add styling to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Spotify Auth Example</h1>
+        <h1>Welcome to Wrapify</h1>
         <button onClick={() => window.location = 'http://localhost:5000/login'}>Log in with Spotify</button>
       </header>
     </div>
